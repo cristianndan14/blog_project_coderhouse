ce8f14e683b7a92ba2534cc97f66fb5e4d1e191d
Cristian Ance <cristianndan14@gmail.com>
2022-06-27 03:13:56 -0300

    add account views, forms, html and model

--- blog_project/accounts/urls.py
+++ blog_project/accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from accounts import views
 
 
-
+app_name='account'
 urlpatterns = [
-    
+    path('login', views.login_request, name='account-login'),
+    path('logout', views.logout_request, name='account-logout'),
+    path('register', views.register, name='account-register'),
+    path('register/update', views.account_update, name='account-update'),
+    path('avatar/load', views.avatar_load, name='avatar-load'),
 ]
